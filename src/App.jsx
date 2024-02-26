e1e7f7eca7f2758b775e7a6c0e7d73d1a50fcdaf
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-02-26 12:16:24 +0545

    db initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <div>
+      <h1>Blogs app</h1>
+    </div>
+  );
+}
+
+export default App;
